b8ddb29c87c12e52f7400e5eaed20d0391184155
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-12-04 13:44:24 +0000

    updated base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 This module containing the BaseModel class.
 """
 
-
+import models
 import uuid
 from datetime import datetime
 
@@ -22,9 +22,16 @@ class BaseModel:
         - updated_at (datetime): Assigns current datetime an instance is
                 created and updates every time the object is changed.
         """
+        form = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, form)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """
@@ -53,5 +60,5 @@ class BaseModel:
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
         obj_dict["__class__"] = self.__class__.__name__
-        
+
         return obj_dict
