5342528c205e17cf2ada257314af5da3bf0618ca
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-12-04 18:27:54 +0000

    Going to task 7

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for the entry point of the command interpreter."""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Define the hbnb command interpreter class."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit the command interpreter."""
+        return True
+
+    def do_EOF(self, arg):
+        """Handle EOF character to quit the command interpreter."""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
