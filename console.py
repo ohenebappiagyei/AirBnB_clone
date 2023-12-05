369348dcf3d795fa03cd293680e002d0202deca2
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2023-12-05 12:41:46 +0000

    Moving on to task 7

--- console.py
+++ console.py
@@ -21,5 +21,5 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing when an empty line is entered."""
         pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
